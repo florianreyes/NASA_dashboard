af292c48a1472902a2963d2e3b03ec73f16e71f3
Florian <Dominique@Florian.fibertel.com.ar>
2023-02-20 03:13:49 -0300

    Add graphs for past week average velocity and diameter of NEOWs

--- nasa.py
+++ nasa.py
@@ -7,30 +7,29 @@ date = datetime.today()
 API_KEY = "cKHzDHu797xhVuCX2Z25x38Qj6ZAtlowWg8DnXYP"
 URL_APOD = "https://api.nasa.gov/planetary/apod?api_key={}".format(API_KEY)
 URL_NEOWS = "https://api.nasa.gov/neo/rest/v1/feed?start_date={}&end_date={}&api_key={}".format(str(date-timedelta(days = 7)).split()[0],str(date).split()[0],API_KEY)
+URL_MARS = "https://api.nasa.gov/insight_weather/?api_key={}&feedtype=json&ver=1.0".format(API_KEY)
 
 
-def get_nasa_apod():
-    response = requests.get(URL_APOD)
+def get_data(url):
+    response = requests.get(url)
     return response.json()
 
-def get_nasa_neows():
-    response = requests.get(URL_NEOWS)
-    return response.json()
-
-def make_neows_dataframe():
-    data = get_nasa_neows()
+def make_neows_dataframe(data):
     bodies = []
     for date in data['near_earth_objects']:
         for i in range(len(data['near_earth_objects'][date])):
-            bodies.append([date,data['near_earth_objects'][date][i]['name'],data['near_earth_objects'][date][i]['estimated_diameter']['meters']['estimated_diameter_max'],data['near_earth_objects'][date][i]['close_approach_data'][0]['miss_distance']['kilometers'],data['near_earth_objects'][date][i]['close_approach_data'][0]['relative_velocity']['kilometers_per_hour'],data['near_earth_objects'][date][i]['close_approach_data'][0]['orbiting_body'],data['near_earth_objects'][date][i]['is_potentially_hazardous_asteroid']])
+            bodies.append([date,data['near_earth_objects'][date][i]['name'],float(data['near_earth_objects'][date][i]['estimated_diameter']['meters']['estimated_diameter_max']),float(data['near_earth_objects'][date][i]['close_approach_data'][0]['miss_distance']['kilometers']),float(data['near_earth_objects'][date][i]['close_approach_data'][0]['relative_velocity']['kilometers_per_hour']),data['near_earth_objects'][date][i]['close_approach_data'][0]['orbiting_body'],data['near_earth_objects'][date][i]['is_potentially_hazardous_asteroid']])
     df = pd.DataFrame(bodies,columns = ['Date','Name','Diameter','Miss_distance','Velocity','Orbiting_body','Hazardous'])
     return df
-    
+
 def get_hazardous_proportion(data):
     return data[data['Hazardous'] == True].shape[0]/data.shape[0]
 
+def get_mars_weather():
+    response = requests.get(URL_MARS)
+    return response.json()
 
-if __name__ == "__main__":
-    datita = make_neows_dataframe()
-    print(datita.head(10))
+
+
+    
    
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,34 @@
 import streamlit as st
 import pandas as pd
-from nasa import make_neows_dataframe, get_hazardous_proportion
+from nasa import make_neows_dataframe, get_hazardous_proportion,URL_NEOWS, get_data
+
+data_asteroids = make_neows_dataframe(get_data(URL_NEOWS))
 
 with open('style.css') as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
-data_asteroids = make_neows_dataframe()
 
-st.title("NASA informative dashboard")
+st.title("🌎 NASA informative dashboard")
 st.markdown('''---''')
 
 #Row A
 st.markdown(''' ## Near Earth Object Metrics''')
 col1, col2, col3 = st.columns(3)
 col1.metric("Hazardous Asteroid Proportion", str(round(get_hazardous_proportion(data_asteroids)*100,2)) + "%")
-col2.metric("Average Velocity", str(round(pd.to_numeric(data_asteroids['Velocity']).mean(),2)) + " km")
-col3.metric("Average Diameter", str(round(pd.to_numeric(data_asteroids['Diameter']).mean(),2)) + " km")
+col2.metric("Average Velocity", str(round(data_asteroids['Velocity'].mean())) + " km")
+col3.metric("Average Diameter", str(round(data_asteroids['Diameter'].mean(),2)) + " km")
 st.markdown('''---''')
+
+#Row B
+
+c1,c2 = st.columns(2)
+data_diameter = data_asteroids.groupby(data_asteroids['Date'])['Diameter'].mean()
+data_velocity = data_asteroids.groupby(data_asteroids['Date'])['Velocity'].mean()
+
+with c1: 
+    st.markdown(''' ### Average Diameter (km)''')
+    st.line_chart(data_diameter)
+
+with c2: 
+    st.markdown(''' ### Average Velocity (km/h)''')
+    st.line_chart(data_velocity)
